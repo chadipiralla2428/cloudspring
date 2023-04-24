bc9fc6af861b1b7c59426e77b3bd6ec2168d763d
chadipiralla2428 <schadipiralla@gmail.com>
2023-04-24 10:39:05 +0530

    controller changes

--- src/main/java/com/example/awsdemo/AwsController.java
+++ src/main/java/com/example/awsdemo/AwsController.java
@@ -2,15 +2,16 @@ package com.example.awsdemo;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/awsdemo")
 public class AwsController {
-	@GetMapping()
-	public String awsDemo() {
-		return "Welcome to Aws Devops";
+	@GetMapping("/{name}")
+	public String awsDemo(@PathVariable String name) {
+		return "Welcome to Aws Devops:"+ name;
 	}
 
 }
